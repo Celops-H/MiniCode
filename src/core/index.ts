0b5bb10a0f72f5404ea9fa57cc08194b2cd41acb
Celops <celops@mail.ustc.edu.cn>
2026-08-10 01:06:23 +0800

    feat: 新增统一消息模型（core/，三类消息 + 内容块）

---
+++ src/core/index.ts
@@ -0,0 +1,18 @@
+export {
+  userMessage,
+  assistantMessage,
+  toolResultMessage,
+  toolCallsOf,
+} from "./message.js";
+export type {
+  ContentBlock,
+  TextContent,
+  ThinkingContent,
+  ToolCall,
+  UserMessage,
+  AssistantMessage,
+  AssistantMeta,
+  ModelUsage,
+  ToolResultMessage,
+  Message,
+} from "./message.js";
